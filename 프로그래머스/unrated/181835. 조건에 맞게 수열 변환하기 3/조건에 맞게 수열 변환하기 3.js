540eb6f66e93dbede04a47ebaa8592475efe725b
Ramon K. Shin <51011193+ramer-dev@users.noreply.github.com>
2023-05-28 17:09:19 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 3, Time: 5.88 ms, Memory: 47.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
@@ -0,0 +1,11 @@
+function solution(arr, k) {
+    var answer = [];
+    for (let i of arr){
+        if(k % 2 === 0){
+            answer.push(i+k)
+        } else {
+            answer.push(i*k)
+        }
+    }
+    return answer;
+}
